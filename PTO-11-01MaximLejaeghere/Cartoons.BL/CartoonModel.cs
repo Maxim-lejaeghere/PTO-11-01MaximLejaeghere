544459fa4b81d78e8c5c5be2fd7a0f681cbea7bd
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2020-04-26 17:41:13 +0200

    First push

---
+++ PTO-11-01MaximLejaeghere/Cartoons.BL/CartoonModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cartoons.BL
+{
+    public class CartoonModel
+    {
+		StudioModel Studio = new StudioModel();
+
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+
+		private string _image;
+
+		public string Image
+		{
+			get { return _image; }
+			set { _image = value; }
+		}
+
+
+		private DateTime _dateCreation;
+
+		public DateTime DateCreation
+		{
+			get { return _dateCreation; }
+			set { _dateCreation = value; }
+		}
+
+		public CartoonModel(string name, string image, DateTime dateCreation, StudioModel studio)
+		{
+			Name = name;
+			Image = image;
+			DateCreation = dateCreation;
+			Studio = studio;
+		}
+		
+
+		public CartoonModel()
+		{
+
+		}
+
+		public void GetName()
+		{
+
+		}
+
+		public void GetDateOfCreation()
+		{
+
+		}
+
+		public void GetImage()
+		{
+
+		}
+
+		public virtual void GetStudio()
+		{
+
+		}
+
+		public void SetName()
+		{
+
+		}
+
+		public void SetDateOfCreation()
+		{
+
+		}
+
+		public void SetImage()
+		{
+
+		}
+
+		public virtual void SetStudio()
+		{
+
+		}
+
+		public void AddCartoon()
+		{
+
+		}
+
+		public virtual void GetCartoons()
+		{
+			
+		}
+	}
+}
---
+++ PTO-11-01MaximLejaeghere/Cartoons.BL/StudioModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cartoons.BL
+{
+    public class StudioModel : CartoonModel
+    {
+		CartoonModel NewCartoon = new CartoonModel();
+
+		private string _studio;
+
+		public string Studio
+		{
+			get { return _studio; }
+			set { _studio = value; }
+		}
+
+		private List<CartoonModel> _cartoonList = new List<CartoonModel>();
+
+		public List<CartoonModel> CartoonList
+		{
+			get { return _cartoonList; }
+			set { _cartoonList = value; }
+		}
+
+		public StudioModel(string studio)
+		{
+
+		}
+
+		public StudioModel()
+		{
+
+		}
+
+		public override void GetStudio()
+		{
+
+		}
+
+		public override void SetStudio()
+		{
+
+		}
+		
+		public override void GetCartoons()
+		{
+
+		}
+
+		public void SetCartoons()
+		{
+			CartoonList.Add(new CartoonModel("mickey mouse", "Images/cat-mickey-mouse.jpg", new DateTime(2000, 10, 15), new StudioModel("Walt Disney")));
+			CartoonList.Add(new CartoonModel("Arnold", "Images/Arnold.jpg", new DateTime(1980, 03, 31), new StudioModel("Nickelodeon")));
+			CartoonList.Add(new CartoonModel("Buzz Lightyear", "Images/cat-mickey-mouse.jpg", new DateTime(2000, 10, 15), new StudioModel("Pixar")));
+		}
+	}
+}
